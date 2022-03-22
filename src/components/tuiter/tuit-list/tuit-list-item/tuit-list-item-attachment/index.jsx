8bc61919a7530e498d4fa594289b89683ede0d08
Shashwat <shashwatrathod22@gmail.com>
2022-03-22 18:49:31 -0400

    a7: created tuit list

---
+++ src/components/tuiter/tuit-list/tuit-list-item/tuit-list-item-attachment/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+//TODO: maintain styles
+const TuitListItemAttachment = ({ tuit }) => {
+  return (
+    <>
+      {tuit.attachments && tuit.attachments.image && (
+        <img
+          src={tuit.attachments.image}
+          className="mt-2 wd-border-radius-20px"
+          style={{ width: "100%" }}
+          alt={tuit.title}
+        />
+      )}
+      {tuit.attachments && tuit.attachments.video && (
+        <iframe
+          width="100%"
+          height="350px"
+          className="mt-2 wd-border-radius-20px"
+          style={{ width: "100%" }}
+          src={`https://www.youtube.com/embed/${tuit.attachments.video}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      )}
+    </>
+  );
+};
+
+export default TuitListItemAttachment;
---
+++ src/components/tuiter/tuit-list/tuit-list-item/tuit-stats/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./tuit-stats.css";
+
+const TuitStats = ({ tuit }) => {
+  return (
+    <>
+      <div className="d-flex justify-content-between px-2 wd-text-muted">
+        <div>
+          <i className="far fa-comment"></i>
+          <span className="ms-2">{tuit.stats.comments}</span>
+        </div>
+        <div>
+          <i class="fas fa-retweet"></i>
+          <span className="ms-2">{tuit.stats.retuits}</span>
+        </div>
+        <div>
+          {tuit.liked ? (
+            <i className="fas fa-heart wd-tuit-liked"></i>
+          ) : (
+            <i className="far fa-heart"></i>
+          )}
+          <span className="ms-2">{tuit.stats.likes}</span>
+        </div>
+        <div>
+          <i className="fas fa-external-link-alt"></i>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TuitStats;
