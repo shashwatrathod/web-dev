a022865f1d90a0b0a36475a360abf1d8c1b7b932
Shashwat <shashwatrathod22@gmail.com>
2022-03-22 16:39:52 -0400

    a7: refactored routes !BROKEN UI!

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ function App() {
     <BrowserRouter>
       <div className="container">
         <Routes>
-          <Route path="hello" exact={true} element={<HelloWorld />} />
-          <Route path="/" exact={true} element={<Labs />} />
-          <Route path="tuiter" exact={true} element={<Tuiter />} />
-          <Route path="tuiter/home" element={<HomeScreen />} />
-          <Route path="tuiter/explore" element={<ExploreScreen />} />
+          <Route path="/">
+            <Route path="labs" element={<Labs />} />
+            <Route path="hello" element={<HelloWorld />} />
+            <Route path="tuiter" element={<Tuiter />}>
+              <Route index element={<HomeScreen />} />
+              <Route path="explore" element={<ExploreScreen />} />
+            </Route>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
