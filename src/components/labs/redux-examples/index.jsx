342a0599051be539f62c11f14893f7c00d0ea653
Shashwat <shashwatrathod22@gmail.com>
2022-03-22 14:58:59 -0400

    a7: todos reducer

--- src/components/labs/redux-examples/index.jsx
+++ src/components/labs/redux-examples/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HelloReduxExampleComponent from "./hello-redux-example-component";
 import hello from "./reducers/hello";
-import { createStore } from "redux";
+import todos from "./reducers/todos-reducer";
+import { combineReducers, createStore } from "redux";
 import { Provider } from "react-redux";
+import Todos from "./todos-component";
 
-const store = createStore(hello);
+const combinedReducers = combineReducers({ hello, todos });
+
+const store = createStore(combinedReducers);
 
 const ReduxExamples = () => {
   return (
@@ -12,6 +16,7 @@ const ReduxExamples = () => {
       <Provider store={store}>
         <div>
           <h2>Redux Examples</h2>
+          <Todos />
           <HelloReduxExampleComponent />
         </div>
       </Provider>
